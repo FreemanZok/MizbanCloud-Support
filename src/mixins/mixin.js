c158bde697a408e5cdd3d7736ae7525f97b27866
ali zokaei <ali.zokaei.1367@gmail.com>
2024-03-12 00:32:38 +0330

    add mixin

---
+++ src/mixins/mixin.js
@@ -0,0 +1,40 @@
+import helpers from "@/plugins/helpers";
+export function myMixin() {
+	function showAlert(message) {
+		alert(message);
+	}
+	function changePriceLastThreeDigits(price) {
+		const roundedPrice = Math.floor(price / 1000) * 1000;
+		return roundedPrice;
+	}
+	function copyToClipboard(text) {
+		navigator.clipboard
+			.writeText(text)
+			.then(() => {
+				helpers.showAlert("متن مورد نظر کپی شد.", "موفق", "success");
+			})
+			.catch((error) => {
+				console.error("Failed to copy text:", error);
+			});
+	}
+	const FormatNumber3Digit = (number) => {
+		let formattedAmount = number.replace(/[^0-9]/g, "");
+		return formattedAmount;
+	};
+	const changeToArray = (inObject, inArray) => {
+		Object.keys(inObject).forEach((element, idx) => {
+			let el = {};
+			el = { ...inObject[element] };
+			el.element_id = element;
+			inArray.push(el);
+		});
+	};
+
+	return {
+		FormatNumber3Digit,
+		showAlert,
+		copyToClipboard,
+		changeToArray,
+		changePriceLastThreeDigits,
+	};
+}
